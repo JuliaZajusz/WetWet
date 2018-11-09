58ca6b548cac8dbb4ce31b004c0af62f7f53ada3
234986 <37815979+234986@users.noreply.github.com>
2018-11-09 20:07:56 +0100

    test commit

--- reservationservice-app/src/main/resources/example_data.sql
+++ reservationservice-app/src/main/resources/example_data.sql
@@ -1,3 +1,4 @@
 insert into position (Type) values("receptionist");
 insert into employee (First_Name, Last_Name, Position_ID) values ("Jan", "Kowalski", 1);
+insert into employee (First_Name, Last_Name, Position_ID) values ("Andrzej", "Wolej", 1);
 
