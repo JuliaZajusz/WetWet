c18a5e846a7447031e3d60305d3edb6d68f0b9d5
Julia Zajusz <julia.zajusz@fingo.pl>
2019-01-13 14:07:50 +0100

    tests - shouldFindExistingPatient

--- reservationservice-app/src/test/java/com/wetwet/ReservationService/ReservationServiceApplicationTests.java
+++ reservationservice-app/src/test/java/com/wetwet/ReservationService/ReservationServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.wetwet.ReservationService;
 
+import com.wetwet.ReservationService.service.PatientService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ReservationServiceApplicationTests {
 
+    @Autowired
+    private PatientService patientService;
+
 	@Test
 	public void contextLoads() {
 	}
+
+
+//	@Test
+//	public void shouldFindExistingPatient() {
+//		// when
+//		PatientDTO patient = patientService.getPatientById(1);
+//		// then
+//		assertThat(patient).isNotNull();
+//	}
 
 }
